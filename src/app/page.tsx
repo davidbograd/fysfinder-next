cd8159df166f9f239e6f0fb135b571df4c25ebf6
David <David@Davids-MacBook-Pro.local>
2024-09-30 21:35:53 +0800

    padding tweak

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -89,7 +89,7 @@ export default async function HomePage() {
     return (
       <div>
         <Header totalClinics={clinics.length} />
-        <div className="max-w-4xl mx-auto px-2">
+        <div className="max-w-4xl mx-auto px-4">
           <SuburbList suburbs={suburbs} />
         </div>
       </div>
