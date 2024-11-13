150d8568a04b3e5cf01e2075bab368ac6a8e6a86
David <David@Davids-MacBook-Pro.local>
2024-11-13 17:13:45 +0800

    navigation update

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -4,28 +4,30 @@ import { Button } from "@/components/ui/button";
 
 export default function Header() {
   return (
-    <header className="bg-white p-4 mb-2">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between items-center">
+    <header className="bg-white p-2 sm:p-4 mb-2">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col sm:flex-row sm:justify-between sm:items-center gap-2">
         <Link href="/" className="inline-block">
           <SiteLogo />
         </Link>
-        <nav className="flex items-center space-x-6">
-          <Link href="/" className="text-gray-600 hover:text-gray-900">
-            Find fysioterapeut
+        <nav className="flex items-center space-x-4 sm:space-x-6">
+          <Link
+            href="/"
+            className="text-sm sm:text-base text-gray-600 hover:text-gray-900"
+          >
+            Lokationer
+          </Link>
+          <Link
+            href="/specialer"
+            className="text-sm sm:text-base text-gray-600 hover:text-gray-900"
+          >
+            Specialer
           </Link>
           <Link
             href="/fysioterapeut-artikler"
-            className="text-gray-600 hover:text-gray-900"
+            className="text-sm sm:text-base text-gray-600 hover:text-gray-900"
           >
             Artikler
           </Link>
-          {/* <Link href="/om-os" className="text-gray-600 hover:text-gray-900">
-            Om os
-          </Link> */}
-          {/* 
-          <Button variant="default" asChild>
-            <Link href="/tilfoej-klinik">Tilføj din klinik</Link>
-          </Button> */}
         </nav>
       </div>
     </header>
