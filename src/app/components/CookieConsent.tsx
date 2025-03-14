c4408c8c0ca42057cfae1b811dd9bb3878d0f630
David <david.bjorn.bograd@gmail.com>
2025-03-14 13:50:01 +0800

    cookie banner tweaks

--- src/app/components/CookieConsent.tsx
+++ src/app/components/CookieConsent.tsx
@@ -24,6 +24,8 @@ export function CookieConsentBanner() {
       window.gtag?.("consent", "update", {
         analytics_storage: "granted",
         ad_storage: "granted",
+        ad_user_data: "granted",
+        ad_personalization: "granted",
       });
     }
   };
@@ -34,6 +36,8 @@ export function CookieConsentBanner() {
       window.gtag?.("consent", "update", {
         analytics_storage: "denied",
         ad_storage: "denied",
+        ad_user_data: "denied",
+        ad_personalization: "denied",
       });
     }
   };
