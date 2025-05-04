3df668bbe338b7e69b6bb68243a87f33df0aadc0
David <david.bjorn.bograd@gmail.com>
2025-05-04 16:30:54 +0800

    pdf ai test

---
+++ src/app/bare-en-test/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function BareEnTestPage() {
+  return (
+    <div>
+      <h1>Bare en test</h1>
+      <iframe
+        src="https://pdf.ai/share/embed/cma5sbrk30018js0dm9op4t7r"
+        loading="lazy"
+        frameBorder="0"
+        scrolling="no"
+        width="100%"
+        height="800px"
+        title="PDF Embed"
+      ></iframe>
+    </div>
+  );
+}
